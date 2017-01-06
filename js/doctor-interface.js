7b457b4fb6a27402000caf56aa5b3902ad2afaef
ryanpeterson08 <ryanpeterson08@gmail.com>
2017-01-06 09:25:40 -0800

    add front end logic so user can search by name

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -6,11 +6,23 @@ $(document).ready(function() {
   $('form#condition').submit(function(event){
     $("ul").text("");
     var userInput = $('#user-condition').val();
+
+    doctorInstance.searchByCondition(userInput);
+    doctorInstance.doctorsArray.forEach(function(doctor){
+      //console.log(doctor);
+      $("ul").append("<li>" + doctor.first_name + " " + doctor.last_name +"</li>" + "<img src=" + doctor.image_url + ">");
+    });
+    event.preventDefault();
+  });
+  
+  $('form#doctor-name').submit(function(event){
+    $("ul").text("");
+    var userInput = $('#user-doctor').val();
     //$('#user-input').val("");
 
-    doctorInstance.getDoctors(userInput);
+    doctorInstance.searchByName(userInput);
     doctorInstance.doctorsArray.forEach(function(doctor){
-      console.log(doctor);
+      //console.log(doctor);
       $("ul").append("<li>" + doctor.first_name + " " + doctor.last_name +"</li>" + "<img src=" + doctor.image_url + ">");
     });
     event.preventDefault();
--- js/doctor.js
+++ js/doctor.js
@@ -5,7 +5,7 @@ function Doctor()
   this.doctorsArray = [];
 }
 
-Doctor.prototype.getDoctors = function(userIssue)
+Doctor.prototype.searchByCondition = function(userIssue)
 {
   var instance = this;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
