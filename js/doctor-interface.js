8ad5e7e49dca735131d6ad84109a96a04af8c67c
Ryan Peterson <ryanpeterson08@gmail.com>
2017-01-05 20:26:29 -0800

    fix bug - wasn't clearing array in backend, so doctors kept getting pushed to array, without it emptying first

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,17 +1,26 @@
 //var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+function displayList(doctorInstance)
+{
+  // doctorInstance.doctorsArray.forEach(function(doctor){
+  //   //console.log(doctor.first_name + " " + doctor.last_name);
+  //   $('#show-doctors').append("first name " + doctor.first_name + " last name " + doctor.last_name);
+  // });
+};
+
+var doctorInstance = new Doctor();
 $(document).ready(function() {
-  var doctorInstance = new Doctor();
   $('form#input').submit(function(event){
-    event.preventDefault();
-
+    $("ul").text("");
     var userInput = $('#user-input').val();
-    doctorInstance.getDoctors(userInput);
+    //$('#user-input').val("");
 
+    doctorInstance.getDoctors(userInput);
     doctorInstance.doctorsArray.forEach(function(doctor){
-      //console.log(doctor.first_name + " " + doctor.last_name);
-      $('#show-doctors').append("<h4>" + doctor.first_name + " " + doctor.last_name + "</h4");
+      console.log(doctor.first_name + " " + doctor.last_name);
+      $("ul").append("<li>" + doctor.first_name + " " + doctor.last_name + "</li>");
     });
+    event.preventDefault();
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -10,6 +10,7 @@ Doctor.prototype.getDoctors = function(userIssue)
   var instance = this;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
+     instance.doctorsArray = [];
      for(var x = 0; x < result.data.length; x++)
      {
        instance.doctorsArray.push(result.data[x].profile);
