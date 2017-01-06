61ce2f027063d631e5a45aa0404954469dc2739d
ryanpeterson08 <ryanpeterson08@gmail.com>
2017-01-06 09:14:31 -0800

    add styling and include doctor images

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,25 +1,17 @@
-//var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-function displayList(doctorInstance)
-{
-  // doctorInstance.doctorsArray.forEach(function(doctor){
-  //   //console.log(doctor.first_name + " " + doctor.last_name);
-  //   $('#show-doctors').append("first name " + doctor.first_name + " last name " + doctor.last_name);
-  // });
-};
 
 var doctorInstance = new Doctor();
 $(document).ready(function() {
-  $('form#input').submit(function(event){
+  $('form#condition').submit(function(event){
     $("ul").text("");
-    var userInput = $('#user-input').val();
+    var userInput = $('#user-condition').val();
     //$('#user-input').val("");
 
     doctorInstance.getDoctors(userInput);
     doctorInstance.doctorsArray.forEach(function(doctor){
-      console.log(doctor.first_name + " " + doctor.last_name);
-      $("ul").append("<li>" + doctor.first_name + " " + doctor.last_name + "</li>");
+      console.log(doctor);
+      $("ul").append("<li>" + doctor.first_name + " " + doctor.last_name +"</li>" + "<img src=" + doctor.image_url + ">");
     });
     event.preventDefault();
   });
--- js/doctor.js
+++ js/doctor.js
@@ -15,11 +15,24 @@ Doctor.prototype.getDoctors = function(userIssue)
      {
        instance.doctorsArray.push(result.data[x].profile);
      }
-      //console.log(instance.doctorsArray);
     })
    .fail(function(error){
       console.log("fail");
     });
+};
+
+Doctor.prototype.searchByName = function(name)
+{
+  var instance = this;
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?name=' + name +'&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=' + apiKey).then(function(result){
+    instance.doctorsArray = [];
+    for(var x = 0; x < result.data.length; x++)
+    {
+      instance.doctorsArray.push(result.data[x].profile);
+    }
+  }).fail(function(error){
+    console.log("fail");
+  });
 };
 
 exports.doctorModule = Doctor;
