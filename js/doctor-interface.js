4780b0015b46d86e3b216630dd28c07673b8ac5a
Ryan Peterson <ryanpeterson08@gmail.com>
2017-01-05 19:00:18 -0800

    add backend function that grabs list of doctors

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,9 +1,12 @@
 //var apiKey = require('./../.env').apiKey;
-var Weather = require('./../js/doctor.js').doctorModule;
+var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
+  var doctorInstance = new Doctor();
   $('form#input').submit(function(event){
     event.preventDefault();
-    
+
+    var userInput = $('#user-input').val();
+    doctorInstance.getDoctors(userInput);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,9 +1,19 @@
-function Doctor(){
+var apiKey = require('./../.env').apiKey;
 
+function Doctor()
+{
+  this.doctorsArray = [];
 }
 
-Doctor.prototype.getDoctors = function(){
-
-}
+Doctor.prototype.getDoctors = function(userIssue)
+{
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result.data[0].profile);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
 
 exports.doctorModule = Doctor;
